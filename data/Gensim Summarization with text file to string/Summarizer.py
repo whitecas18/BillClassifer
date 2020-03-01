d1982c336ad2cd5da1b0670ccfc2f5a7f5b7b8a3
Shay Merchant <merchants18@students.ecu.edu>
2020-03-01 22:54:29 +0000

    Replace Summarizer.py

--- data/Gensim Summarization with text file to string/Summarizer.py
+++ data/Gensim Summarization with text file to string/Summarizer.py
@@ -1,10 +1,20 @@
 import gensim
+import re
 from gensim.summarization.summarizer import summarize
 from gensim.summarization.textcleaner import split_sentences
 
-
+# convert text from file to a string
 with open("congress.txt", "r") as myfile:
-    text = str(myfile.readlines())
-    
+    text = (str(myfile.readlines()))
 
-print(summarize((text), .1))
+# remove number chars
+text = re.sub('\d', '', text)
+# remove certain special chars
+text = text.replace('--', '').replace('  ','').replace('(', '').replace(')','').replace('$','')
+
+# create text file
+File_object = open("newFile.txt","w")
+
+# write to text file
+File_object.writelines(summarize(text , .05))
+print(summarize(text ,.05))
