b7ab8ebf8f63f0e3c4c7df0ce9ab173f98b6b105
Dillon Roberts <dillon.w.roberts@gmail.com>
2020-03-03 20:54:37 -0500

    Fixes to stemmer, reduced default verbosity in report

--- src/preprocess/tokenizer.py
+++ src/preprocess/tokenizer.py
@@ -35,7 +35,7 @@ import pathlib
 from collections import Counter, defaultdict
 
 class d_model:
-	def __init__(self, d_path, path_stopwords=None):
+	def __init__(self, d_path, out_path=None, path_stopwords=None):
 		# d_path: 				path to data
 		# path_stopwords:	Path to a text file of stopwords
 		self.data_path = d_path
@@ -43,6 +43,7 @@ class d_model:
 		self.stopwords = set()
 		if path_stopwords is None:
 			self.stopwords = set(stopwords.words('english'))
+		self.out_path = out_path
 
 		self.vocab = set()
 		self.total_word_counts = 0
@@ -69,36 +70,48 @@ class d_model:
 		files = [f for f in glob.glob(self.data_path + "\\*")]
 		for file in files:
 			file = open(file, 'r')
-			self.preprocess(file.read(), only_tokenize)
+			self.tokenize_doc(file.read(), only_tokenize)
 			file.close()		
-
-		self.stem()
 		self.update_model()
-	def stem(self):
+		self.stem()
+		self.remove_sw()
+		self.update_model()
+
+	def stem(self, minChars = 3):
 		# Stem all the words in the vocabulary.
 		# Removes the original word from the word counts
 		# and adds it to the count of the stemmed word.
-		ps = nltk.LancasterStemmer()
-		for word in self.vocab:
-			stemmed = ps.stem(word)
-			self.word_counts[stemmed] += self.word_counts.pop(word, None)
+		ps = nltk.PorterStemmer()
+		for word in list(self.vocab):
 			self.vocab.remove(word)
-			self.vocab.add(stemmed)
+			if len(word) < minChars:		
+				self.total_word_counts -= self.word_counts.pop(word, None)
+			else:
+				stemmed = ps.stem(word)
+				count = self.word_counts.pop(word, None) 
+				self.word_counts[stemmed] += count # This seems mundane, but necessary to prevent double counting
+				self.vocab.add(stemmed)
+
+		self.update_model()
 		
-	def preprocess(self, doc, only_tokenize=False):
+	def remove_sw(self):
+		stopwords = self.stopwords
+		for stopword in stopwords:
+			self.total_word_counts -= self.word_counts.pop(stopword, 0.0)
+		self.vocab.difference_update(stopwords)
+
+	def tokenize_doc(self, doc, only_tokenize=False):
 		# Given a doc and a flag to only tokenize
 		# process the tokens of the doc
 		tokens = doc.split()
 		
 		# Replace uppercase with lowercase and remove all punctuation + digits
-		replace_table = str.maketrans(string.ascii_uppercase,string.ascii_lowercase, string.punctuation + string.digits)
+		symbols = '‘-'
+		replace_table = str.maketrans(string.ascii_uppercase,string.ascii_lowercase, symbols + string.punctuation + string.digits)
 		for token in tokens:
 			self.word_counts[token.translate(replace_table)]+=1.0
 		# Some strings become empty, it's faster to not check and delete them at the end
-		self.total_word_counts -= self.word_counts.pop("", 0.0)
-		stopwords = self.stopwords
-		for stopword in stopwords:
-			self.word_counts.pop(stopword, 0.0)
+		self.word_counts.pop("", 0.0)
 	
 	def sliceCorpusEnds(self, percentage=0.10):
 		# Remove the top and bottom percentage of words
@@ -135,32 +148,38 @@ class d_model:
 		self.vocab.update(self.word_counts.keys())
 		self.total_word_counts += sum(self.word_counts.values())
 
-	def report(self):
+	def report(self, show_word_lists=False):
 		print ("-------Corpus Stats-------")
 
-		print ("Token Count: ", sum(self.word_counts.values()))
+		print ("Token Count: ", self.total_word_counts)
 		print ("Vocabulary Size: ", len(self.vocab))
+		if(show_word_lists):
+			top_words = [x[0] for x in self.word_counts.most_common(50)]
+			print("50 Most Frequently Occuring Words: ")
+			s_words = ', '.join(top_words)
+			wrapper = textwrap.TextWrapper(40, break_long_words=False)
+			print(wrapper.fill(s_words))
 
-		top_words = [x[0] for x in self.word_counts.most_common(50)]
-		print("50 Most Frequently Occuring Words: ")
-		s_words = ', '.join(top_words)
-		wrapper = textwrap.TextWrapper(30, break_long_words=False)
-		print(wrapper.fill(s_words))
+			sw_words = ''
+			for word in top_words:
+				if word in self.stopwords:
+					sw_words = sw_words + ',' + word
+			if len(sw_words)>0:
+				print('Stopwords in top 20 word counts:', wrapper.fill(sw_words[1:]))
+			else:
+				print('No stopwords in top 20 most occuring words')
+#	def save_to_disc(self):
+#		outFile = 
+#		for word in self.vocab:
 
-		sw_words = ''
-		for word in top_words:
-			if word in self.stopwords:
-				sw_words = sw_words + ',' + word
-		if len(sw_words)>0:
-			print('Stopwords in top 20 word counts:', wrapper.fill(sw_words[1:]))
-		else:
-			print('No stopwords in top 20 most occuring words')
+
 
 #region		
 # If for some reason you cannot run from command line arguments
 # edit these variables to path for their respective locations.
 d_preprocess = "src/preprocess/"
 d_path_data = d_preprocess + "NLP CORPUS TXT/"
+d_out_path = d_path_data
 f_path_stopwords = d_preprocess + "stopwords.txt"
 
 if len(sys.argv) > 1:
@@ -172,20 +191,22 @@ print("Path Locations: ")
 print("\tData Path: ", (pathlib.Path(d_path_data).absolute() ))
 print("\tStopwords File Path:", (pathlib.Path(((f_path_stopwords))).absolute()))
 #endregion
-print("Raw Corpus:")
 #pr = cProfile.Profile()
 #pr.enable()
 
-model = d_model(d_path_data)
+model = d_model(d_path_data, out_path=d_out_path)
 model.process()
+print("Post Processing")
 model.report()
 
 initTokens = model.total_word_counts
 initVocab = len(model.vocab)
 
 model.sliceCorpusEnds(percentage=0.10)
-model.report()
+print("Post Slice")
+model.report(show_word_lists=True)
 print("Removed", str(initTokens - model.total_word_counts), "tokens and " + \
 	 str(initVocab - len(model.vocab)) + " vocab words")
+
 #pr.print_stats(sort='time')
 #pr.disable()
